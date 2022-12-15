b26c905e365af41a2281406585a199870399442a
shmd001 <cool.shoronov19@gmail.com>
2022-12-16 02:08:04 +0300

    Added basic frontend

---
+++ src/frontend/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <div className="topnav">
+                <a href="#bc">Home</a>
+                <a href="#bc">Blockchain</a>
+            </div>
+            <div className="center-screen">
+                <h1>BLOCKCHAIN</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
